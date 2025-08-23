a01d92f88ce5dac09dcd11bfdd58c91a42918878
Benjamin Teh <bejamintehjh01@gmail.com>
2025-08-23 10:16:42 +0800

    reorg

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -70,5 +70,13 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Set tabs
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Color 81st character
+vim.o.colorcolumn = '81'
 
 -- vim: ts=2 sts=2 sw=2 et
