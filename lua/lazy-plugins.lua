f888742ddba12c1a9a24db9b5bef6b15c0350192
Benjamin Teh <bejamintehjh01@gmail.com>
2025-11-01 09:59:41 +0800

    add copilot

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -45,6 +45,8 @@ require('lazy').setup({
 
     require 'plugins.live-preview',
 
+    require 'plugins.copilot',
+
     -- The following comments only work if you have downloaded the repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- place them in the correct locations.
